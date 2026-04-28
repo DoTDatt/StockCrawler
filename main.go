37c1d217764315d2b557b2a2f841e97dc4b6edc1
ThanhDat <panhhuni@gmail.com>
2026-04-28 15:39:25 +0700

    feat: add stock crawler with batch upsert

--- main.go
+++ main.go
@@ -1,89 +1,200 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"net/url"
+	"strings"
 	"time"
 
-	"github.com/xuri/excelize/v2"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/robfig/cron/v3"
+)
+
+const (
+	dsn        = "root:root@tcp(127.0.0.1:3306)/vtv_index_news_db2?charset=utf8mb4&parseTime=True&loc=Local"
+	apiURL     = "https://api.hsx.vn/l/api/v1/1/securities/stock"
+	pageSize   = 50
+	maxRetries = 3
+	delay      = 500 * time.Millisecond
 )
 
 type StockItem struct {
-	Name string `json:"name"`
-	Code string `json:"code"`
+	Name    string `json:"name"`
+	Brief   string `json:"brief"`
+	Address string `json:"address"`
+	Phone   string `json:"phone"`
+	Fax     string `json:"fax"`
+	WebUrl  string `json:"webUrl"`
+	Capital uint64 `json:"capital"`
+	Isin    string `json:"isin"`
 }
 
 type Paging struct {
 	TotalPages int `json:"totalpages"`
 }
 
-type StockRespone struct {
-	Data struct {
-		List   []StockItem `json:"list"`
-		Paging Paging      `json:"paging"`
-	} `json:"data"`
+type Data struct {
+	List   []StockItem `json:"list"`
+	Paging Paging      `json:"paging"`
 }
 
-func main() {
-	client := &http.Client{}
-	f := excelize.NewFile()
-	sheet := "Sheet1"
+type StockResponse struct {
+	Data Data `json:"data"`
+}
 
-	headers := []string{"STT", "Tên đầy đủ", "Mã số doanh nghiệp", "Sàn niêm yết", "Mã chứng khoán", "Nơi cấp"}
-	for i, h := range headers {
-		cell, _ := excelize.CoordinatesToCellName(i+1, 1)
-		f.SetCellValue(sheet, cell, h)
+func fetchWithRetry(ctx context.Context, url string) (*StockResponse, error) {
+	client := &http.Client{Timeout: 10 * time.Second}
+	for i := 0; i < maxRetries; i++ {
+		req, _ := http.NewRequestWithContext(ctx, "GET", url, nil)
+		req.Header.Set("User-Agent", "Mozilla/5.0")
+		resp, err := client.Do(req)
+		if err == nil && resp.StatusCode == 200 {
+			defer resp.Body.Close()
+			body, _ := io.ReadAll(resp.Body)
+			var data StockResponse
+			if err := json.Unmarshal(body, &data); err != nil {
+				return nil, err
+			}
+			return &data, nil
+		}
+		time.Sleep(5 * time.Second) //
 	}
-	row := 2
+	return nil, fmt.Errorf("failed to fetch")
+}
+
+func runCrawler() {
+	log.Println(">>> Start crawling")
+	ctx := context.Background()
+
 	totalPages := 1
 	for page := 1; page <= totalPages; page++ {
-		url := fmt.Sprintf("https://api.hsx.vn/l/api/v1/1/securities/stock?pageIndex=%d&pageSize=30&alphabet=&sectorId=", page)
-
-		req, _ := http.NewRequest("GET", url, nil)
-		req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
-		req.Header.Set("Accept", "application/json")
-
-		res, err := client.Do(req)
+		u, _ := url.Parse(apiURL)
+		q := u.Query()
+		q.Set("pageIndex", fmt.Sprintf("%d", page))
+		q.Set("pageSize", fmt.Sprintf("%d", pageSize))
+		u.RawQuery = q.Encode()
+		finalUrl := u.String()
+		data, err := fetchWithRetry(ctx, finalUrl)
 		if err != nil {
-			panic(err)
-		}
-		defer res.Body.Close()
-
-		body, err := io.ReadAll(res.Body)
-		if err != nil {
-			panic(err)
-		}
-
-		var apiRes StockRespone
-		if err := json.Unmarshal(body, &apiRes); err != nil {
-			fmt.Println("lỗi giải mã")
-			panic(err)
+			log.Printf("Lỗi fetch trang %d: %v", page, err)
+			continue
 		}
 
 		if page == 1 {
-			totalPages = apiRes.Data.Paging.TotalPages
+			totalPages = data.Data.Paging.TotalPages
 		}
 
-		for i, item := range apiRes.Data.List {
-
-			f.SetCellValue(sheet, fmt.Sprintf("A%d", row), row-1)
-			f.SetCellValue(sheet, fmt.Sprintf("B%d", row), item.Name)
-			// f.SetCellValue(sheet, fmt.Sprintf("C%d", row), item.BusinessRegNo)
-			// f.SetCellValue(sheet, fmt.Sprintf("D%d", row), item.ListingBoard)
-			f.SetCellValue(sheet, fmt.Sprintf("E%d", row), item.Code)
-			// f.SetCellValue(sheet, fmt.Sprintf("F%d", row), item.Isi)
-			row++
-			_ = i
+		err = upsertToDB(data.Data.List)
+		if err != nil {
+			log.Printf("Lỗi Upsert trang %d: %v", page, err)
+		} else {
+			log.Printf("Page %d/%d", page, totalPages)
 		}
-		time.Sleep(300 * time.Millisecond)
-		fmt.Printf("page: %d, totalPages: %d, items: %d\n", page, totalPages, len(apiRes.Data.List))
+
+		time.Sleep(delay)
 	}
-	if err := f.SaveAs("stock.xlsx"); err != nil {
-		panic(err)
+	log.Println(">>> End : total")
+}
+
+func upsertToDB(items []StockItem) error {
+	if len(items) == 0 {
+		return nil
 	}
 
-	fmt.Println("Done export Excel")
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
 
+	defer tx.Rollback()
+
+	var companyValues []string
+	var companyArray []any
+	var translationValues []string
+	var translationArgs []any
+
+	for _, item := range items {
+		if item.Brief == "" || item.Name == "" {
+			continue
+		}
+
+		idno := item.Isin
+		if idno == "" {
+			idno = item.Brief
+		}
+		slug := strings.ToLower(strings.ReplaceAll(item.Name, " ", "-"))
+
+		companyValues = append(companyValues, "(?, ?, ?, ?, ?, ?, NULL, ?, 'HOSE', 'active', 'congtydaichung')")
+
+		companyArray = append(companyArray,
+			idno,
+			item.Brief,
+			item.Address,
+			item.Phone,
+			item.Fax,
+			item.WebUrl,
+			item.Capital,
+		)
+
+		translationValues = append(translationValues, "(?,'vi'?,?,null)")
+		translationArgs = append(translationArgs, idno, item.Name, slug)
+
+	}
+
+	companyUpsertQuery := `
+		INSERT INTO companies 
+		(id_no, short_name, address, telephone, fax, website, email, capital, exchange, status, type)
+		VALUES` + strings.Join(companyValues, ",") + `
+		ON DUPLICATE KEY UPDATE
+		address = VALUES(address),
+		telephone = VALUES(telephone),
+		capital = VALUES(capital),
+		updated_at = NOW()
+		`
+	tx.Exec(companyUpsertQuery, companyArray...)
+
+	companyTranslationsUpsertQuery := `
+		INSERT INTO company_translations
+		(company_id, lang_code, name, slug, description)
+		VALUES ` + strings.Join(translationValues, ",") + `
+		ON DUPLICATE KEY UPDATE
+		name = VALUES(name),
+		slug = VALUES(slug)	
+		`
+
+	tx.Exec(companyTranslationsUpsertQuery, translationArgs...)
+
+	return tx.Commit()
+}
+
+var db *sql.DB
+
+func main() {
+
+	var err error
+
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal("Lỗi kết nối DB:", err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal("DB không phản hồi:", err)
+	}
+	runCrawler()
+	c := cron.New()
+	c.AddFunc("@every 30s", func() {
+		runCrawler()
+	})
+	c.Start()
+
+	log.Println("--- Start ---")
+	select {}
 }
